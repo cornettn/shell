c6cd71c718baf6308b2b424bebd1133fe1f13dcf
Noah Liam Cornett <cornettn@data.cs.purdue.edu>
2019-09-25 23:30:24 -0400

    Latest build

--- shell.c
+++ shell.c
@@ -72,7 +72,7 @@ int main() {
   struct sigaction sa_zombies;
   sa_zombies.sa_handler = sig_child_handler;
   sigemptyset(&sa_zombies.sa_mask);
-  sa_zombies.sa_flags = SA_RESTART;
+  sa_zombies.sa_flags = SA_RESTART|SA_NOCLDSTOP|SA_NOCLDWAIT;
   int zombie = sigaction(SIGCHLD, &sa_zombies, NULL);
 
   if (zombie) {
