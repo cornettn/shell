f50a3eafa098611eb273befea64e82422e8ab781
Noah Liam Cornett <cornettn@data.cs.purdue.edu>
2019-09-30 13:53:49 -0400

    Latest build

--- command.c
+++ command.c
@@ -82,8 +82,8 @@ int execute_builtin(command_t *command) {
   single_command_t *single = command->single_commands[0];
   if (!strcmp(single->arguments[0], "exit")) {
     free_command(g_current_command);
-    if (g_single_current_command) {
-      free_single_command(g_single_current_command);
+    if (g_current_single_command) {
+      free_single_command(g_current_single_command);
     }
     if (command != g_current_command) {
       free_command(command);
