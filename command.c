8d18adef3282d0f40a9a81e72bac393ec1927cea
Noah Liam Cornett <cornettn@data.cs.purdue.edu>
2019-09-30 13:45:28 -0400

    Latest build

--- command.c
+++ command.c
@@ -81,7 +81,10 @@ void change_directory(char *dir) {
 int execute_builtin(command_t *command) {
   single_command_t *single = command->single_commands[0];
   if (!strcmp(single->arguments[0], "exit")) {
-    free_command(command);
+    free_command(g_current_command);
+    if (command != g_current_command) {
+      free_command(command);
+    }
     exit(1);
   }
   else if (!strcmp(single->arguments[0], "printenv")) {
