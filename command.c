4073d7a629b267ac460405fbdd889dfa7c6e6446
Noah Liam Cornett <cornettn@data.cs.purdue.edu>
2019-10-03 15:59:49 -0400

    Latest build

--- command.c
+++ command.c
@@ -313,7 +313,7 @@ int set_fd_err(command_t *command, int default_err) {
  */
 
 void execute_command(command_t *command) {
-
+  g_last_command = command;
   g_debug = open("debug", O_CREAT|O_RDWR|O_APPEND, 0600);
 
   /* Don't do anything if there are no single commands */
