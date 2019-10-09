80a0089400879df140f219443ce5a38b22f692e0
Noah Liam Cornett <cornettn@data.cs.purdue.edu>
2019-10-09 10:05:59 -0400

    Latest build

--- y.tab.c
+++ y.tab.c
@@ -460,7 +460,7 @@ char **find_matching_strings(char **array, DIR *dir, regex_t reg,
 int my_compare(const void *a, const void *b) {
   char *str_a = *(char **) a;
   char *str_b = *(char **) b;
-  return strcasecmp(str_a, str_b);
+  return strcmp(str_a, str_b);
 }
 
 
