778c6dbb08847df700e93bc6e41233310d1867e9
Noah Liam Cornett <cornettn@data.cs.purdue.edu>
2019-10-13 13:55:03 -0400

    Latest build

--- read_line.c
+++ read_line.c
@@ -83,6 +83,13 @@ char *read_line() {
       // add char to buffer.
       g_line_buffer[insert_pos] = ch;
       g_line_length++;
+
+
+      for (int i = insert_pos; i < g_line_length; i++) {
+        write(1, &g_line_buffer[i], 1);
+      }
+
+
       insert_pos++;
     }
     else if (ch == 10) {
