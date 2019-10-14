e33f7c77d78f7da31df00e5ff9ee4eee1b004ed2
Noah Liam Cornett <cornettn@data.cs.purdue.edu>
2019-10-14 10:29:55 -0400

    Latest build

--- read_line.c
+++ read_line.c
@@ -164,12 +164,13 @@ char *read_line() {
       g_line_length--;
       insert_pos--;
 
-      // Write a space to erase the last character read
-      ch = ' ';
-      write(1, &ch, 1);
 
       // Go back one character
       ch = 8;
+      write(1, &ch, 1);
+
+      // Write a space to erase the last character read
+      ch = ' ';
       write(1, &ch, 1);
 
       // Go back one character
